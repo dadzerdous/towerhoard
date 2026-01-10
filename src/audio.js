fab26be1df90dd437bd3ddf883032d7f94b76e8a
dadzerdous <dadzaverous@gmail.com>
2026-01-09 20:23:21 -0500

    Update audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,11 +1,20 @@
 const shootSound = new Audio('./assets/sounds/shoot.wav');
 shootSound.volume = 0.4;
 
+// NEW: Load the select sound
+const selectSound = new Audio('./assets/sounds/select.ogg');
+selectSound.volume = 0.6; // Slightly louder than bg elements
+
 export const AudioMgr = {
     playShoot: () => {
-        // Clone allows rapid fire overlapping
         const s = shootSound.cloneNode(); 
         s.volume = 0.4;
+        s.play().catch(e => {});
+    },
+    // NEW: Play Select
+    playSelect: () => {
+        const s = selectSound.cloneNode();
+        s.volume = 0.6;
         s.play().catch(e => {});
     }
 };
